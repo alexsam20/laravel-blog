c0c00720c26447ea564d66a888854e42c90ae592
Alexandr SaM <alexsam2070@gmail.com>
2023-07-30 11:12:25 -0400

    Add link to About Us page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@
             @foreach($categories as $category)
                 <a href="{{ route('by-category', $category) }}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">{{ $category->title }}</a>
             @endforeach
-            <a href="#" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">About us</a>
+            <a href="{{ route('about-us') }}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">About us</a>
         </div>
     </div>
 </nav>
