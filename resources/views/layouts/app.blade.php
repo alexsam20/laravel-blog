9e9726ae861e044e746ee2451b53866323f07881
Alexandr SaM <alexsam2070@gmail.com>
2023-08-05 23:31:41 -0400

    Add metaTitle and metaDescription to app.blade template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>alexsers Blog</title>
-    <meta name="author" content="">
-    <meta name="description" content="">
+    <title>{{ $metaTitle ?: 'alexsers Blog' }}</title>
+    <meta name="author" content="alexsers">
+    <meta name="description" content="{{ $metaDescription }}">
 
     <!-- Tailwind -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
