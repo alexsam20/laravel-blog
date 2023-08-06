eedeb73e08c50d5bab5c143c1cafd2b58f5281c8
Alexandr SaM <alexsam2070@gmail.com>
2023-08-06 18:58:25 -0400

    Combined Breeze with the templates and routes

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -2,16 +2,35 @@
 
 namespace App\View\Components;
 
+use App\Models\Category;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\Component;
-use Illuminate\View\View;
 
 class AppLayout extends Component
 {
-    /**
-     * Get the view / contents that represents the component.
-     */
-    public function render(): View
+    public Collection $categories;
+
+    public function __construct(public ?string $metaTitle = null, public ?string $metaDescription = null)
     {
-        return view('layouts.app');
+        $this->categories = Category::query()
+            ->join('category_post', 'categories.id', '=', 'category_post.category_id')
+            ->select('categories.title', 'categories.slug', DB::raw('count(*) as total'))
+            ->groupBy('categories.id')
+            ->orderByDesc('total')
+            ->limit(5)
+            ->get();
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        $categories = $this->categories;
+
+        return view('layouts.app', compact('categories'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -29,3 +31,8 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('/about-us', [SiteController::class, 'about'])->name('about-us');
+Route::get('/category/{category:slug}', [PostController::class, 'byCategory'])->name('by-category');
+Route::get('/{post:slug}', [PostController::class, 'show'])->name('view');
