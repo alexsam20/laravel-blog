6a29b9d750806a5d1ef0115ea464b981bcbda3c3
Alexandr SaM <alexsam2070@gmail.com>
2023-08-06 08:41:30 -0400

    Add style and links to templates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,14 @@
         .font-family-karla {
             font-family: karla;
         }
+
+        pre {
+            padding: 1rem;
+            background-color: #1a202c;
+            color: white;
+            border-radius: 0.5rem;
+            margin: 0.7rem 0.7rem;
+        }
     </style>
 
     <!-- AlpineJS -->
@@ -22,11 +30,11 @@
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
 </head>
-<body class="bg-white font-family-karla">
+<body class="bg-gray-50 font-family-karla">
 
 <header class="w-full container mx-auto">
     <div class="flex flex-col items-center py-12">
-        <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="#">
+        <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="{{ route('home') }}">
             Alexsers Blog
         </a>
         <p class="text-lg text-gray-600">
