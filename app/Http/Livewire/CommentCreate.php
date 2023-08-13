99d04d60192eb0d3c4dc126306f6cb73530c4e11
Alexandr SaM <alexsam2070@gmail.com>
2023-08-13 12:53:43 -0400

    Create Comments to Post page

---
+++ app/Http/Livewire/CommentCreate.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Comment;
+use App\Models\Post;
+use Livewire\Component;
+
+class CommentCreate extends Component
+{
+    public string $comment = '';
+
+    public Post $post;
+
+    public function mount(Post $post): void
+    {
+        $this->post = $post;
+    }
+
+    public function render()
+    {
+        return view('livewire.comment-create');
+    }
+
+    public function createComment()
+    {
+        $user = auth()->user();
+        if (!$user) {
+            return $this->redirect('/login');
+        }
+        $comment = Comment::create([
+            'comment' => $this->comment,
+            'post_id' => $this->post->id,
+            'user_id' => $user->id,
+        ]);
+
+        $this->emitUp('commentCreated', $comment->id);
+    }
+}
---
+++ resources/views/livewire/comment-create.blade.php
@@ -0,0 +1,15 @@
+<div>
+   <div x-data="{
+        focused:false
+   }" class="mb-4">
+       <div class="mb-2">
+           <textarea wire:model="comment" @click="focused = true" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-insert ring-gray-300 placeholder-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" :rows="focused ? '2' : '1'" placeholder="Leave a comment"></textarea>
+       </div>
+       <div :class="focused ? '' : 'hidden'">
+           <button wire:click="createComment" type="submit" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2">
+               Submit
+           </button>
+           <button @click="focused = false" type="button" class="">Cancel</button>
+       </div>
+   </div>
+</div>
